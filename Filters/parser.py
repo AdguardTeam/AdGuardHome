6f21251e95a9f9af803705eb38ed50c247ca0bdb
nkartyshov <nikkartyshov@gmail.com>
2016-07-07 10:28:04 +0300

    Did one set of filters for DNS

--- Filters/parser.py
+++ Filters/parser.py
@@ -14,22 +14,32 @@ def save_comment(comment, f):
        comment = comment[:idx] + date_now() + '\n'
     f.writelines(comment)    
 
-def save_rule(url, f):
-    url = line.replace('include', '').strip() 
-    for rule in get_content(url):
-        rule = rule.replace('^', '')
-        idx = rule.find('$third-party')        
-        if idx != -1:
+def save_url_rule(url, f):
+    url = line.replace('url', '').strip() 
+    for rule in get_content(url):        
+        if rule.find('^') != -1:
+            idx = rule.find('^')         
             f.writelines(rule[:idx] + '\n')
+        elif rule.find('$') != -1:
+            idx = rule.find('$');
+            f.writelines(rule[:idx] + '\n')        
         else: 
             f.writelines(rule + '\n')    
+
+def save_file_rule(line, f):
+    file_name = line.replace('file', '').strip()
+    with open(file_name, 'r') as rf:
+        for rule in rf:
+            f.writelines(rule + '\n')
 
 with open('filter.template', 'r') as tmpl:    
     with open('filter.txt', 'w') as f:
        for line in tmpl:
             if line.startswith('!'):
                 save_comment(line, f)
-            if line.startswith('include'):
-                save_rule(line, f)
+            if line.startswith('url'):
+                save_url_rule(line, f)
+            if line.startswith('file'):
+                save_file_rule(line, f)
                 
                     
