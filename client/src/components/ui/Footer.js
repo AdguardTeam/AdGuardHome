97161ab4f0694367458933bc6ba44efe4d5e0509
Eugene Bujak <hmage@hmage.net>
2018-09-10 20:57:52 +0300

    web interface -- Update footer from placeholder to actual values.

--- client/src/components/ui/Footer.js
+++ client/src/components/ui/Footer.js
@@ -14,13 +14,10 @@ class Footer extends Component {
                         <div className="col-12 col-lg-auto ml-lg-auto">
                             <ul className="list-inline list-inline-dots text-center mb-0">
                                 <li className="list-inline-item">
-                                    <a href="https://adguard.com/welcome.html" target="_blank" rel="noopener noreferrer">Homepage</a>
+                                    <a href="https://adguard.com/adguard-dns/overview.html" target="_blank" rel="noopener noreferrer">Homepage</a>
                                 </li>
                                 <li className="list-inline-item">
-                                    <a href="https://github.com/AdguardTeam/" target="_blank" rel="noopener noreferrer">Github</a>
-                                </li>
-                                <li className="list-inline-item">
-                                    <a href="https://adguard.com/privacy.html" target="_blank" rel="noopener noreferrer">Privacy Policy</a>
+                                    <a href="https://github.com/AdguardTeam/AdguardDNS" target="_blank" rel="noopener noreferrer">Github</a>
                                 </li>
                             </ul>
                         </div>
