bdaea88bf0b79d84da717393a447f71bd78e3cff
Ildar Kamalov <i.kamalov@adguard.com>
2018-10-15 13:49:07 +0300

    Add "FAQ" link to the header
    Closes #370

--- client/src/components/Header/Menu.js
+++ client/src/components/Header/Menu.js
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types';
 import enhanceWithClickOutside from 'react-click-outside';
 import classnames from 'classnames';
 
+import { REPOSITORY } from '../../helpers/constants';
+
 class Menu extends Component {
     handleClickOutside = () => {
         this.props.closeMenu();
@@ -52,6 +54,12 @@ class Menu extends Component {
                                 <svg className="nav-icon" fill="none" height="24" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg"><path d="m14 2h-8a2 2 0 0 0 -2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-12z"/><path d="m14 2v6h6"/><path d="m16 13h-8"/><path d="m16 17h-8"/><path d="m10 9h-1-1"/></svg>
                                 Query Log
                             </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <a href={`${REPOSITORY.URL}/wiki`} className="nav-link" target="_blank" rel="noopener noreferrer">
+                                <svg className="nav-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#66b574" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="10"></circle><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"></path><line x1="12" y1="17" x2="12" y2="17"></line></svg>
+                                FAQ
+                            </a>
                         </li>
                     </ul>
                 </div>
