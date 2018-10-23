98bf5322a36b173bc8949934848f4974d29bf31d
Mo Mustakim Ali <mustakimali@users.noreply.github.com>
2018-10-23 13:48:51 +0100

    Using culture-specific approach instead of regex

--- client/src/components/Filters/index.js
+++ client/src/components/Filters/index.js
@@ -50,7 +50,7 @@ class Filters extends Component {
         Header: 'Rules count',
         accessor: 'rulesCount',
         className: 'text-center',
-        Cell: props => props.value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ','),
+        Cell: props => new Number(props.value).toLocaleString(),
     }, {
         Header: 'Last time updated',
         accessor: 'lastUpdated',
