60a35d9aa8107a80d7819be4dc528d95164080c5
rob passmore <rpassmore@gmail.com>
2019-01-18 19:24:40 +0000

    Configure IP address dns server binds to

--- dns.go
+++ dns.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"net"
-	"strings"
 
 	"github.com/AdguardTeam/AdGuardHome/dnsfilter"
 	"github.com/AdguardTeam/AdGuardHome/dnsforward"
@@ -32,19 +31,8 @@ func generateServerConfig() dnsforward.ServerConfig {
 		})
 	}
 
-	ief, err := net.InterfaceByName(config.DNS.BindInterface)
-	if err != nil {
-		log.Fatal(err)
-	}
-	addrs, err := ief.Addrs()
-	if err != nil {
-		log.Fatal(err)
-	}
-	externalIpAddr := addrs[0].String()[:strings.Index(addrs[0].String(), "/")]
-	log.Println("DNS Binding if:ip ", config.DNS.BindInterface + ":" + externalIpAddr)
-
 	newconfig := dnsforward.ServerConfig{
-		UDPListenAddr:   &net.UDPAddr{IP: net.ParseIP(externalIpAddr), Port: config.DNS.Port},
+		UDPListenAddr:   &net.UDPAddr{IP: net.ParseIP(config.DNS.BindHost), Port: config.DNS.Port},
 		FilteringConfig: config.DNS.FilteringConfig,
 		Filters:         filters,
 	}
