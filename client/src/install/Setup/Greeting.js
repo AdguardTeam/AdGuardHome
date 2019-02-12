35b5f4b48b53b281e41434988d21bc49234ced12
Ildar Kamalov <i.kamalov@adguard.com>
2019-02-12 10:31:37 +0300

    Fixed json and updated zh_tw

--- client/src/install/Setup/Greeting.js
+++ client/src/install/Setup/Greeting.js
@@ -1,23 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Trans, withNamespaces } from 'react-i18next';
 import Controls from './Controls';
 
-class Greeting extends Component {
-    render() {
-        return (
-            <div className="setup__step">
-                <div className="setup__group">
-                    <h1 className="setup__title">
-                        <Trans>install_welcome_title</Trans>
-                    </h1>
-                    <p className="setup__desc text-center">
-                        <Trans>install_welcome_desc</Trans>
-                    </p>
-                </div>
-                <Controls />
-            </div>
-        );
-    }
-}
+const Greeting = () => (
+    <div className="setup__step">
+        <div className="setup__group">
+            <h1 className="setup__title">
+                <Trans>install_welcome_title</Trans>
+            </h1>
+            <p className="setup__desc text-center">
+                <Trans>install_welcome_desc</Trans>
+            </p>
+        </div>
+        <Controls />
+    </div>
+);
 
 export default withNamespaces()(Greeting);
