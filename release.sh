110434c2d53567b480c918ff181d2b68879be875
Eugene Bujak <hmage@hmage.net>
2019-02-12 21:31:03 +0300

    Fix broken tar.gz not having a subdirectory inside.

--- release.sh
+++ release.sh
@@ -9,16 +9,19 @@ version=`git describe --abbrev=4 --dirty --always --tags`
 f() {
 	make cleanfast; CGO_DISABLED=1 make
 	if [[ $GOOS == darwin ]]; then
+	    rm -f dist/AdGuardHome_"$version"_MacOS.zip
 	    zip dist/AdGuardHome_"$version"_MacOS.zip AdGuardHome README.md LICENSE.txt
 	elif [[ $GOOS == windows ]]; then
+	    rm -f dist/AdGuardHome_"$version"_Windows.zip
 	    zip dist/AdGuardHome_"$version"_Windows.zip AdGuardHome.exe README.md LICENSE.txt
 	else
-	    tar zcvf dist/AdGuardHome_"$version"_"$GOOS"_"$GOARCH".tar.gz AdGuardHome README.md LICENSE.txt
+	    pushd ..
+	    tar zcvf AdGuardHome_"$version"_"$GOOS"_"$GOARCH".tar.gz AdGuardHome/{AdGuardHome,LICENSE.txt,README.md}
+	    popd
 	fi
 }
 
 # Prepare the dist folder
-rm -rf dist
 mkdir -p dist
 
 # Prepare releases
