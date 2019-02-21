37a1a98c49a91c2a6948474b9d8ed8fbebaf5bab
Ildar Kamalov <i.kamalov@adguard.com>
2019-02-21 15:39:15 +0300

    Fixed EncryptionTopline check

--- client/src/components/ui/EncryptionTopline.js
+++ client/src/components/ui/EncryptionTopline.js
@@ -8,27 +8,29 @@ import Topline from './Topline';
 import { EMPTY_DATE } from '../../helpers/constants';
 
 const EncryptionTopline = (props) => {
-    if (props.notAfter !== EMPTY_DATE) {
-        const isAboutExpire = isAfter(addDays(Date.now(), 30), props.notAfter);
-        const isExpired = isAfter(Date.now(), props.notAfter);
+    if (props.notAfter === EMPTY_DATE) {
+        return false;
+    }
 
-        if (isExpired) {
-            return (
-                <Topline type="danger">
-                    <Trans components={[<a href="#settings" key="0">link</a>]}>
-                        topline_expired_certificate
-                    </Trans>
-                </Topline>
-            );
-        } else if (isAboutExpire) {
-            return (
-                <Topline type="warning">
-                    <Trans components={[<a href="#settings" key="0">link</a>]}>
-                        topline_expiring_certificate
-                    </Trans>
-                </Topline>
-            );
-        }
+    const isAboutExpire = isAfter(addDays(Date.now(), 30), props.notAfter);
+    const isExpired = isAfter(Date.now(), props.notAfter);
+
+    if (isExpired) {
+        return (
+            <Topline type="danger">
+                <Trans components={[<a href="#settings" key="0">link</a>]}>
+                    topline_expired_certificate
+                </Trans>
+            </Topline>
+        );
+    } else if (isAboutExpire) {
+        return (
+            <Topline type="warning">
+                <Trans components={[<a href="#settings" key="0">link</a>]}>
+                    topline_expiring_certificate
+                </Trans>
+            </Topline>
+        );
     }
 
     return false;
