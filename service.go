faa7c7b2d4ca6ce7062a85bdf75ff8ad7c1c9166
Simon Zolin <s.zolin@adguard.com>
2019-04-17 19:57:42 +0300

    + service install: a post-install guide of what to do next

--- service.go
+++ service.go
@@ -108,6 +108,15 @@ func handleServiceControlAction(action string) {
 				log.Fatalf("Failed to start the service: %s", err)
 			}
 			log.Printf("Service has been started")
+
+			if detectFirstRun() {
+				log.Printf(`Almost ready!
+AdGuard Home is successfully installed and will automatically start on boot.
+There are a few more things that must be configured before you can use it.
+Click on the link below and follow the Installation Wizard steps to finish setup.`)
+				printHTTPAddresses("http")
+			}
+
 		} else if action == "uninstall" {
 			cleanupService()
 		}
