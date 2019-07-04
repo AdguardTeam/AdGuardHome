98ff11e1c781a373768f01c54f6c7c29d8096d32
Simon Zolin <s.zolin@adguard.com>
2019-07-04 14:26:34 +0300

    - freebsd: fix build
    Go's "syscall" package file for FreeBSD (incorrectly?) uses int64
     types in syscall.Rlimit struct.

--- home/os_freebsd.go
+++ home/os_freebsd.go
@@ -1,4 +1,4 @@
-// +build aix darwin dragonfly linux netbsd openbsd solaris
+// +build freebsd
 
 package home
 
@@ -13,8 +13,8 @@ import (
 // https://github.com/AdguardTeam/AdGuardHome/issues/659
 func setRlimit(val uint) {
 	var rlim syscall.Rlimit
-	rlim.Max = uint64(val)
-	rlim.Cur = uint64(val)
+	rlim.Max = int64(val)
+	rlim.Cur = int64(val)
 	err := syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rlim)
 	if err != nil {
 		log.Error("Setrlimit() failed: %v", err)
