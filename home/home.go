0311e53ebba1ccdc0026f4327ef748cbe6340d94
Andrey Meshkov <am@adguard.com>
2020-05-08 17:43:49 +0300

    *: added comment

--- home/home.go
+++ home/home.go
@@ -336,6 +336,8 @@ func requireAdminRights() {
 	admin, _ := util.HaveAdminRights()
 	if //noinspection ALL
 	admin || isdelve.Enabled {
+		// Don't forget that for this to work you need to add "delve" tag explicitly
+		// https://stackoverflow.com/questions/47879070/how-can-i-see-if-the-goland-debugger-is-running-in-the-program
 		return
 	}
 
