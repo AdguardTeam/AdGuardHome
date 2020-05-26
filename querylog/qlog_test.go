61a24ffc71eda26e10a11e3a9c8506909b6c4b52
Andrey Meshkov <am@adguard.com>
2020-05-26 16:24:31 +0300

    *(querylog): decode methods to a new file

--- querylog/qlog_test.go
+++ querylog/qlog_test.go
@@ -183,33 +183,6 @@ func TestQueryLogMaxFileScanEntries(t *testing.T) {
 	assert.Equal(t, 10, len(entries))
 }
 
-func TestJSON(t *testing.T) {
-	s := `
-	{"keystr":"val","obj":{"keybool":true,"keyint":123456}}
-	`
-	k, v, jtype := readJSON(&s)
-	assert.Equal(t, jtype, int32(jsonTStr))
-	assert.Equal(t, "keystr", k)
-	assert.Equal(t, "val", v)
-
-	k, v, jtype = readJSON(&s)
-	assert.Equal(t, jtype, int32(jsonTObj))
-	assert.Equal(t, "obj", k)
-
-	k, v, jtype = readJSON(&s)
-	assert.Equal(t, jtype, int32(jsonTBool))
-	assert.Equal(t, "keybool", k)
-	assert.Equal(t, "true", v)
-
-	k, v, jtype = readJSON(&s)
-	assert.Equal(t, jtype, int32(jsonTNum))
-	assert.Equal(t, "keyint", k)
-	assert.Equal(t, "123456", v)
-
-	k, v, jtype = readJSON(&s)
-	assert.True(t, jtype == jsonTErr)
-}
-
 func addEntry(l *queryLog, host, answerStr, client string) {
 	q := dns.Msg{}
 	q.Question = append(q.Question, dns.Question{
