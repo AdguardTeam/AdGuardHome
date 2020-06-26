5e0fe8ba3f90757691550f4579471ddb872dc0aa
Alan Pope <alan.pope@canonical.com>
2020-06-26 12:33:15 +0100

    Update adguard-home-web.sh
    Add missing space

--- packaging/snap/local/adguard-home-web.sh
+++ packaging/snap/local/adguard-home-web.sh
@@ -2,7 +2,7 @@
 # Get admin tool port from configuration
 bind_port=$(grep bind_port $SNAP_DATA/AdGuardHome.yaml | awk -F ' ' '{print $2}')
 
-if [ -z "$bind_port"]; then
+if [ -z "$bind_port" ]; then
 	xdg-open http://localhost:3000
 else
 	xdg-open http://localhost:$bind_port
