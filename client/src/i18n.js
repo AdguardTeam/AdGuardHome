835f3b911b66902902f4231913913e8bafdde4e4
Andrey Meshkov <am@adguard.com>
2020-09-16 17:42:57 +0300

    Added unused translations counter

--- client/src/i18n.js
+++ client/src/i18n.js
@@ -4,37 +4,39 @@ import langDetect from 'i18next-browser-languagedetector';
 
 import { LANGUAGES, BASE_LOCALE } from './helpers/twosky';
 
-import vi from './__locales/vi.json';
-import en from './__locales/en.json';
-import ru from './__locales/ru.json';
-import es from './__locales/es.json';
-import fr from './__locales/fr.json';
-import ja from './__locales/ja.json';
-import sv from './__locales/sv.json';
-import ptBR from './__locales/pt-br.json';
-import zhTW from './__locales/zh-tw.json';
+import be from './__locales/be.json';
 import bg from './__locales/bg.json';
-import zhCN from './__locales/zh-cn.json';
 import cs from './__locales/cs.json';
 import da from './__locales/da.json';
 import de from './__locales/de.json';
-import id from './__locales/id.json';
-import it from './__locales/it.json';
-import ko from './__locales/ko.json';
-import no from './__locales/no.json';
-import nl from './__locales/nl.json';
-import pl from './__locales/pl.json';
-import ptPT from './__locales/pt-pt.json';
-import sk from './__locales/sk.json';
-import sl from './__locales/sl.json';
-import tr from './__locales/tr.json';
-import srCS from './__locales/sr-cs.json';
+import en from './__locales/en.json';
+import es from './__locales/es.json';
+import fa from './__locales/fa.json';
+import fr from './__locales/fr.json';
 import hr from './__locales/hr.json';
 import hu from './__locales/hu.json';
-import fa from './__locales/fa.json';
-import th from './__locales/th.json';
+import id from './__locales/id.json';
+import it from './__locales/it.json';
+import ja from './__locales/ja.json';
+import ko from './__locales/ko.json';
+import nl from './__locales/nl.json';
+import no from './__locales/no.json';
+import pl from './__locales/pl.json';
+import ptBR from './__locales/pt-br.json';
+import ptPT from './__locales/pt-pt.json';
 import ro from './__locales/ro.json';
+import ru from './__locales/ru.json';
 import siLk from './__locales/si-lk.json';
+import sk from './__locales/sk.json';
+import sl from './__locales/sl.json';
+import srCS from './__locales/sr-cs.json';
+import sv from './__locales/sv.json';
+import th from './__locales/th.json';
+import tr from './__locales/tr.json';
+import vi from './__locales/vi.json';
+import zhCN from './__locales/zh-cn.json';
+import zhHK from './__locales/zh-hk.json';
+import zhTW from './__locales/zh-tw.json';
 import { setHtmlLangAttr } from './helpers/helpers';
 
 const resources = {
@@ -62,11 +64,17 @@ const resources = {
     'pt-br': {
         translation: ptBR,
     },
+    'zh-hk': {
+        translation: zhHK,
+    },
     'zh-tw': {
         translation: zhTW,
     },
     bg: {
         translation: bg,
+    },
+    be: {
+        translation: be,
     },
     'zh-cn': {
         translation: zhCN,
