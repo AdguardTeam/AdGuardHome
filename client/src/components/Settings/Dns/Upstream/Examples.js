a7cf717116a78dbbad086ec149584c7f3c45d33f
KizunaH <63988538+KizunaH@users.noreply.github.com>
2020-10-14 20:24:10 +0700

    Update DNS

--- client/src/components/Settings/Dns/Upstream/Examples.js
+++ client/src/components/Settings/Dns/Upstream/Examples.js
@@ -8,10 +8,10 @@ const Examples = (props) => (
         <Trans>examples_title</Trans>:
         <ol className="leading-loose">
             <li>
-                <code>94.140.14.14</code> - {props.t('example_upstream_regular')}
+                <code>94.140.14.140</code> - {props.t('example_upstream_regular')}
             </li>
             <li>
-                <code>tls://dns.adguard.com</code> –&nbsp;
+                <code>tls://dns-unfiltered.adguard.com</code> –&nbsp;
                 <span>
                     <Trans
                         components={[
@@ -30,7 +30,7 @@ const Examples = (props) => (
                 </span>
             </li>
             <li>
-                <code>https://dns.adguard.com/dns-query</code> –&nbsp;
+                <code>https://dns-unfiltered.adguard.com/dns-query</code> –&nbsp;
                 <span>
                     <Trans
                         components={[
@@ -70,7 +70,7 @@ const Examples = (props) => (
                 </span>
             </li>
             <li>
-                <code>tcp://94.140.14.14</code> – <Trans>example_upstream_tcp</Trans>
+                <code>tcp://94.140.14.140</code> – <Trans>example_upstream_tcp</Trans>
             </li>
             <li>
                 <code>sdns://...</code> –&nbsp;
@@ -108,7 +108,7 @@ const Examples = (props) => (
                 </span>
             </li>
             <li>
-                <code>[/example.local/]94.140.14.14</code> –&nbsp;
+                <code>[/example.local/]94.140.14.140</code> –&nbsp;
                 <span>
                     <Trans
                         components={[
