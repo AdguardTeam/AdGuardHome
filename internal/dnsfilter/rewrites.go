6a09acc2626900fc52873fb0bba3c5978ae319a0
jvoisin <julien.voisin@dustri.org>
2020-12-22 16:43:47 +0100

    Generalise a construct to simplify a function

--- internal/dnsfilter/rewrites.go
+++ internal/dnsfilter/rewrites.go
@@ -122,19 +122,21 @@ func findRewrites(a []RewriteEntry, host string) []RewriteEntry {
 
 	sort.Sort(rr)
 
-	isWC := isWildcard(rr[0].Domain)
-	if !isWC {
-		for i, r := range rr {
-			if isWildcard(r.Domain) {
-				rr = rr[:i]
-				break
-			}
+	for i, r := range rr {
+		if isWildcard(r.Domain) {
+			rr = rr[:max(1, i)]
+			break
 		}
-	} else {
-		rr = rr[:1]
 	}
 
 	return rr
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
 
 func rewriteArrayDup(a []RewriteEntry) []RewriteEntry {
