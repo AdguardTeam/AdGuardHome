b456810f97b6b9900a5feaea5b001356d5e0a754
Rahul Somasundaram <Rahul.Somasundaram@checkpt.com>
2022-09-22 09:34:07 +0530

    [hsts] using forcehttps instead of enabled

--- internal/home/control.go
+++ internal/home/control.go
@@ -310,7 +310,7 @@ func handleHTTPSRedirect(w http.ResponseWriter, r *http.Request) (ok bool) {
 		Scheme: schemeHTTP,
 		Host:   r.Host,
 	}
-	if config.TLS.Enabled {
+	if config.TLS.ForceHTTPS {
 		w.Header().Add("Strict-Transport-Security", "max-age=63072000; includeSubDomains")
 	}
 	w.Header().Set("Access-Control-Allow-Origin", originURL.String())
