3378d1c10304c54f16f1466bd34715975fdbe4df
Ainar Garipov <a.garipov@adguard.com>
2023-02-14 13:52:22 +0300

    Pull request: 5471-new-trackers-db
    Merge in DNS/adguard-home from 5471-new-trackers-db to master
    
    Squashed commit of the following:
    
    commit d90742c2902afd3424fb8721419231493610048b
    Merge: 5e87a1f2 a50a8abb
    Author: Ainar Garipov <A.Garipov@AdGuard.COM>
    Date:   Tue Feb 14 13:38:40 2023 +0300
    
        Merge branch 'master' into 5471-new-trackers-db
    
    commit 5e87a1f25cd1d1dc52629d36e3d50d032ada1093
    Author: Ainar Garipov <A.Garipov@AdGuard.COM>
    Date:   Mon Feb 13 18:38:54 2023 +0300
    
        all: use new tracker source

--- scripts/companiesdb/download.sh
+++ scripts/companiesdb/download.sh
@@ -4,11 +4,9 @@ set -e -f -u -x
 
 # This script syncs companies DB that we bundle with AdGuard Home.  The source
 # for this database is https://github.com/AdguardTeam/companiesdb.
+#
+trackers_url='https://raw.githubusercontent.com/AdguardTeam/companiesdb/main/dist/trackers.json'
+output='./client/src/helpers/trackers/trackers.json'
+readonly trackers_url output
 
-whotracksme='https://raw.githubusercontent.com/AdguardTeam/companiesdb/main/dist/whotracksme.json'
-adguard='https://raw.githubusercontent.com/AdguardTeam/companiesdb/main/dist/adguard.json'
-base_path='../../client/src/helpers/trackers'
-readonly whotracksme adguard base_path
-
-curl -o "${base_path}/whotracksme.json" -v "$whotracksme"
-curl -o "${base_path}/adguard.json"     -v "$adguard"
+curl -o "$output" -v "$trackers_url"
