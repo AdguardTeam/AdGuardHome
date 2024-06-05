cd1be2d66db0a37c853a8336f19e1d5ad03d4803
Igor Lobanov <bniwredyc@gmail.com>
2024-06-05 20:23:14 +0200

    production build quickfix

--- client/src/helpers/twosky.ts
+++ client/src/helpers/twosky.ts
@@ -1,4 +1,6 @@
 // eslint-disable-next-line import/no-relative-packages
 import twosky from '../../../.twosky.json';
 
+console.log(twosky[0]);
+
 export const { languages: LANGUAGES, base_locale: BASE_LOCALE } = twosky[0];
