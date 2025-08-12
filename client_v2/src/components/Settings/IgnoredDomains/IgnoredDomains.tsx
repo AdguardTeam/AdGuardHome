3f6d759b7f6542fc36b203c62b010b35f346b4e6
Ildar Kamalov <ik@adguard.com>
2025-08-12 16:40:31 +0300

    fix placeholder

--- client_v2/src/components/Settings/IgnoredDomains/IgnoredDomains.tsx
+++ client_v2/src/components/Settings/IgnoredDomains/IgnoredDomains.tsx
@@ -71,7 +71,7 @@ export const IgnoredDomains = ({ control, processing, ignoreEnabled, setValue, s
                                 </a>
                             </div>
                         }
-                        placeholder="example.com\n*.example.com\n||example.com^"
+                        placeholder={`example.com\n*.example.com\n||example.com^`}
                         size="large"
                         disabled={processing || !ignoreEnabled}
                         errorMessage={fieldState.error?.message}
