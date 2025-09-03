3bcec663e9127a7a6fbb6a8aef007926f91ee17c
Ainar Garipov <a.garipov@adguard.com>
2025-09-03 17:14:38 +0300

    Pull request 2467: AGDNS-2909-revert-download
    Squashed commit of the following:
    
    commit 198251d40570d807a8156a6a448ac109666f05a2
    Author: Ainar Garipov <A.Garipov@AdGuard.COM>
    Date:   Wed Sep 3 16:57:09 2025 +0300
    
        scripts: try fixing snap download

--- scripts/snap/download.sh
+++ scripts/snap/download.sh
@@ -11,17 +11,17 @@ set -e -f -u
 channel="${CHANNEL:?please set CHANNEL}"
 readonly channel
 
-while read -r arch snap_arch; do
-	release_url="https://static.adtidy.org/adguardhome/${channel}/AdGuardHome_linux_${arch}.tar.gz"
-	output="./AdGuardHome_linux_${arch}.tar.gz"
+printf '%s %s\n' \
+	'386' 'i386' \
+	'amd64' 'amd64' \
+	'armv7' 'armhf' \
+	'arm64' 'arm64' \
+	| while read -r arch snap_arch; do
+		release_url="https://static.adtidy.org/adguardhome/${channel}/AdGuardHome_linux_${arch}.tar.gz"
+		output="./AdGuardHome_linux_${arch}.tar.gz"
 
-	curl -o "$output" -v "$release_url"
-	tar -f "$output" -v -x -z
-	cp ./AdGuardHome/AdGuardHome "./AdGuardHome_${snap_arch}"
-	rm -f -r "$output" ./AdGuardHome
-done <<-'EOF'
-	386   i386
-	amd64 amd64
-	armv7 armhf
-	arm64 arm64
-EOF
+		curl -o "$output" -v "$release_url"
+		tar -f "$output" -v -x -z
+		cp ./AdGuardHome/AdGuardHome "./AdGuardHome_${snap_arch}"
+		rm -f -r "$output" ./AdGuardHome
+	done
