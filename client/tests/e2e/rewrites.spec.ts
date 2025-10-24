6d93c32b465cf17c26beb277d677ca58fe19280a
Ildar Kamalov <ik@adguard.com>
2025-10-24 13:57:47 +0300

    fix test

--- client/tests/e2e/rewrites.spec.ts
+++ client/tests/e2e/rewrites.spec.ts
@@ -1,6 +1,10 @@
 import { test, expect } from '@playwright/test';
 import { ADMIN_PASSWORD, ADMIN_USERNAME } from '../constants';
 
+const EXAMPLE_DOMAIN = `edit.org`;
+const EXAMPLE_UPDATED_DOMAIN = `updated.org`;
+const EXAMPLE_ANSWER = '192.168.1.1';
+
 test.describe('Rewrites', () => {
     test.beforeEach(async ({ page }) => {
         await page.goto('/login.html');
@@ -15,36 +19,23 @@ test.describe('Rewrites', () => {
     });
 
     test('should add a DNS rewrite', async ({ page }) => {
-        const domain = `add.org`;
-        const answer = '127.0.0.1';
-        
         await page.getByTestId('add-rewrite').click();
-        await page.getByTestId('rewrites_domain').fill(domain);
-        await page.getByTestId('rewrites_answer').fill(answer);
+        await page.getByTestId('rewrites_domain').fill(EXAMPLE_DOMAIN);
+        await page.getByTestId('rewrites_answer').fill(EXAMPLE_ANSWER);
         await page.getByTestId('rewrites_save').click();
 
-        await expect(page.locator('.logs__text').filter({ hasText: domain })).toBeVisible();
-        await expect(page.locator('.logs__text').filter({ hasText: answer })).toBeVisible();
+        await expect(page.locator('.logs__text').filter({ hasText: EXAMPLE_DOMAIN })).toBeVisible();
+        await expect(page.locator('.logs__text').filter({ hasText: EXAMPLE_ANSWER })).toBeVisible();
     });
 
     test('should edit a DNS rewrite', async ({ page }) => {
-        const originalDomain = `edit.org`;
-        const updatedDomain = `updated.org`;
-        const answer = '192.168.1.1';
-
-        await page.getByTestId('add-rewrite').click();
-        await page.getByTestId('rewrites_domain').fill(originalDomain);
-        await page.getByTestId('rewrites_answer').fill(answer);
-        await page.getByTestId('rewrites_save').click();
-
         await page.getByTestId('edit-rewrite').first().click();
-        await expect(page.getByTestId('rewrites_domain')).toHaveValue(originalDomain);
+        await expect(page.getByTestId('rewrites_domain')).toHaveValue(EXAMPLE_DOMAIN);
         
         await page.getByTestId('rewrites_domain').clear();
-        await page.getByTestId('rewrites_domain').fill(updatedDomain);
+        await page.getByTestId('rewrites_domain').fill(EXAMPLE_UPDATED_DOMAIN);
         await page.getByTestId('rewrites_save').click();
 
-        await expect(page.locator('.logs__text').filter({ hasText: updatedDomain })).toBeVisible();
-        await expect(page.locator('.logs__text').filter({ hasText: answer })).toBeVisible();
+        await expect(page.locator('.logs__text').filter({ hasText: EXAMPLE_UPDATED_DOMAIN })).toBeVisible();
     });
 });
