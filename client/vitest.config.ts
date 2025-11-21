e44e106b3ceb7d8ebddb776d19c3ef67487614d0
Igor Lobanov <i.lobanov@adguard.com>
2025-11-21 14:48:48 +0100

    build fix

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -1,6 +1,12 @@
+import path from 'path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
     test: {
         environment: 'jsdom',
         include: ['src/__tests__/**'],
