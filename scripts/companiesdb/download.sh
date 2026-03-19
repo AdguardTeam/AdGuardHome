d7b6dc0cf7a13335cb49c9f02f029cafe4d0af23
Maksim Kazantsev <m.kazantsev@adguard.com>
2026-03-19 12:24:04 +0000

    Pull request 2610: ADG-11715-all-update-skel
    Squashed commit of the following:
    
    commit cdb69ce8590c68a77b6d2118cc5df671f4cfcce2
    Author: Maksim Kazantsev <m.kazantsev@adguard.com>
    Date:   Thu Mar 19 12:42:08 2026 +0300
    
        make: rollback redundant changes;
    
    commit 1b7ef9f887b223e8d689fdec8b2595dc2802d2df
    Author: Maksim Kazantsev <m.kazantsev@adguard.com>
    Date:   Thu Mar 19 12:39:28 2026 +0300
    
        make: upd scripts

--- scripts/companiesdb/download.sh
+++ scripts/companiesdb/download.sh
@@ -8,4 +8,4 @@ trackers_url='https://raw.githubusercontent.com/AdguardTeam/companiesdb/main/dis
 output='./client/src/helpers/trackers/trackers.json'
 readonly trackers_url output
 
-curl -v "$trackers_url" | jq . > "$output"
+curl -v "$trackers_url" | jq . >"$output"
