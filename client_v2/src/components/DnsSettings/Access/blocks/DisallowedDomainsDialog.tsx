3f51acf731025fc73077db5b3cf66a9d31b3b9ec
Ildar Kamalov <ik@adguard.com>
2026-07-20 12:04:44 +0300

    highlight disallowed domain comments

--- client_v2/src/components/DnsSettings/Access/blocks/DisallowedDomainsDialog.tsx
+++ client_v2/src/components/DnsSettings/Access/blocks/DisallowedDomainsDialog.tsx
@@ -1,11 +1,11 @@
 import { untrack, type Accessor } from 'solid-js';
-
 import { accessState, setAccessList } from 'panel/stores/access';
 import intl from 'panel/common/intl';
 import { ConfigDialog } from 'panel/common/ui/ConfigDialog';
 import { Textarea } from 'panel/common/controls/Textarea';
 import { useField } from 'panel/hooks/useField';
 import theme from 'panel/lib/theme';
+import { COMMENT_LINE_TOKENS } from 'panel/helpers/constants';
 
 type Props = {
     open: Accessor<boolean>;
@@ -47,6 +47,8 @@ export const DisallowedDomainsDialog = (props: Props) => {
                     label={intl.getMessage('dns_disallowed_domains_label')}
                     size="medium"
                     errorMessage={field.error()}
+                    commentPrefixes={COMMENT_LINE_TOKENS}
+                    highlightComments
                 />
             </div>
         </ConfigDialog>
