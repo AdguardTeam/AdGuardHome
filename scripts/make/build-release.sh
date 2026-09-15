9ab692306367bf1a742aac34dca4b6245dcb6787
Dimitry Kolyshev <dkolyshev@adguard.com>
2026-09-15 10:05:49 +0700

    scripts: rm pipefail

--- scripts/make/build-release.sh
+++ scripts/make/build-release.sh
@@ -29,7 +29,9 @@ readonly sign
 
 # Exit the script if a pipeline fails (-e), prevent accidental filename
 # expansion (-f), and consider undefined variables as errors (-u).
-set -e -o 'pipefail' -f -u
+#
+# TODO(d.kolyshev): Use set -o 'pipefail' when GitHub supports it.
+set -e -f -u
 
 # Function log is an echo wrapper that writes to stderr if the caller requested
 # verbosity level greater than 0.  Otherwise, it does nothing.
