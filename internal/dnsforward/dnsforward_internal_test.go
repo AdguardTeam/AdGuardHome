6a17881293dce51ca064769be61f0311f8827fb0
Maksim Kazantsev <m.kazantsev@adguard.com>
2026-09-21 16:35:40 +0300

    dnsforward: imp tests;

--- internal/dnsforward/dnsforward_internal_test.go
+++ internal/dnsforward/dnsforward_internal_test.go
@@ -1912,13 +1912,29 @@ func TestServer_Exchange(t *testing.T) {
 	})
 }
 
-func TestServer_Ratelimit(t *testing.T) {
+func TestServer_ratelimit(t *testing.T) {
 	t.Parallel()
 
 	const (
 		attemptNum    = 3
 		clientTimeout = 300 * time.Millisecond
 	)
+
+	req := createGoogleATestMessage()
+	filterConf := &filtering.Config{BlockingMode: filtering.BlockingModeDefault}
+	srvConf := ServerConfig{
+		UDPListenAddrs: []*net.UDPAddr{{}},
+		TCPListenAddrs: []*net.TCPAddr{{}},
+		TLSConf:        &TLSConfig{},
+		Config: Config{
+			UpstreamMode:           UpstreamModeLoadBalance,
+			EDNSClientSubnet:       &EDNSClientSubnet{Enabled: false},
+			ClientsContainer:       EmptyClientsContainer{},
+			RatelimitSubnetLenIPv4: netutil.IPv4BitLen,
+			RatelimitSubnetLenIPv6: netutil.IPv6BitLen,
+		},
+		ServePlainDNS: true,
+	}
 
 	testCases := []struct {
 		name      string
@@ -1941,29 +1957,12 @@ func TestServer_Ratelimit(t *testing.T) {
 	}}
 
 	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			t.Parallel()
+		c := srvConf
+		c.Config.Ratelimit = tc.ratelimit
+		c.Config.RatelimitWhitelist = tc.whitelist
 
-			s := createTestServer(
-				t,
-				&filtering.Config{BlockingMode: filtering.BlockingModeDefault},
-				ServerConfig{
-					UDPListenAddrs: []*net.UDPAddr{{}},
-					TCPListenAddrs: []*net.TCPAddr{{}},
-					TLSConf:        &TLSConfig{},
-					Config: Config{
-						UpstreamMode:           UpstreamModeLoadBalance,
-						EDNSClientSubnet:       &EDNSClientSubnet{Enabled: false},
-						ClientsContainer:       EmptyClientsContainer{},
-						Ratelimit:              tc.ratelimit,
-						RatelimitSubnetLenIPv4: netutil.IPv4BitLen,
-						RatelimitSubnetLenIPv6: netutil.IPv6BitLen,
-						RatelimitWhitelist:     tc.whitelist,
-					},
-					ServePlainDNS: true,
-				},
-				testTLSManager,
-			)
+		t.Run(tc.name, func(t *testing.T) {
+			s := createTestServer(t, filterConf, c, testTLSManager)
 			s.conf.UpstreamConfig.Upstreams = []upstream.Upstream{newGoogleUpstream()}
 			startDeferStop(t, s)
 
@@ -1971,8 +1970,6 @@ func TestServer_Ratelimit(t *testing.T) {
 			client := &dns.Client{Net: "udp", Timeout: clientTimeout}
 
 			for i := range attemptNum {
-				req := createGoogleATestMessage()
-
 				reply, _, err := client.Exchange(req, addr)
 
 				if i >= attemptNum-tc.dropNum {
